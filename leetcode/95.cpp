0a53e0c17056459ac96528acbe43bfff2449b1f8
xujinhuan123 <390872892@qq.com>
2025-02-28 14:53:21 +0800

    完成了力扣95

--- leetcode/95.cpp
+++ leetcode/95.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct TreeNode
@@ -11,6 +12,35 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution
+{
+public:
+    vector<TreeNode *> generateTrees(int n)
+    {
+        if (n)
+            return gener(1, n);
+        else
+            return vector<TreeNode *>{};
+    }
+    vector<TreeNode *> gener(int left, int right)
+    {
+        vector<TreeNode *> res;
+        if (left > right)
+        {
+            res.push_back(nullptr);
+            return res;
+        }
+        for (int i = left; i <= right; i++)
+        {
+            vector<TreeNode *> lefts = gener(left, i - 1);
+            vector<TreeNode *> rights = gener(i + 1, right);
+            for (auto left : lefts)
+                for (auto right : rights)
+                    res.push_back(new TreeNode(i, left, right));
+        }
+        return res;
+    }
+};
 int main()
 {
     return 0;
